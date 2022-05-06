5af9719337bfa95c80c1e04540973e7630e19e76
shettimax <32527098+shettimax@users.noreply.github.com>
2022-05-06 20:51:49 +0100

    Add files via upload
    disclaimer

---
+++ disclaimer.php
@@ -0,0 +1,28 @@
+<!-- Dialogged disclaimer -->
+        <div class="row tall-row">
+            <div class="col-lg-12">
+                <h1>Disclaimer</h1>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="modal">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                                <h4 class="modal-title">[!] <u>HEADS UP</u> [!]</h4>
+                            </div>
+                            <div class="modal-body">
+                                <p>------------------------------------------------------<br>We do not promote, encourage, support or excite any illegal activity or hacking without written permission in general. We want you to think outside the box by solving daily ctf challenges. If you plan to use the information for illegal purposes, please leave this website now. We cannot be held responsible for any misuse of your boosted knowlegde.<br>-----------------------------/!\</p>
+                            </div>
+                            <div class="modal-footer">
+                                by: <button type="button" class="btn btn-default" data-dismiss="modal">YWRtaW4=</button>
+                                <a href="home.php"><button type="button" class="btn btn-default" data-dismiss="modal">proceed</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
