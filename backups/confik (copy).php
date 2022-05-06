7f95e8cd0681df6b70ae9cdce62c70c957eb0377
shettimax <32527098+shettimax@users.noreply.github.com>
2022-05-06 20:26:08 +0100

    Add files via upload
    miscels / initials

---
+++ backups/confik (copy).php
@@ -0,0 +1,11 @@
+<?php 
+$host = "localhost"; 
+$user = "root"; 
+$pass = ""; 
+$db = "ctf"; 
+$conn = mysqli_connect($host, $user, $pass);
+mysqli_select_db($conn,$db);
+
+$con= new mysqli('localhost','root','','ctf')or die("Could not connect to mysql".mysqli_error($con));
+
+?>
