e6c4e40957a923697a7a45148d954597c0654b13
shettimax <32527098+shettimax@users.noreply.github.com>
2022-05-06 21:01:58 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<hr>
+<div class="row tall-row">
+            <div class="col-md-12">
+                <p>Created by <a href="https://t.me/mazangizo">0x6d617a616e67697a6f</a>. &copy; 2020-2021</p>
+            </div>
+        </div>
+
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
