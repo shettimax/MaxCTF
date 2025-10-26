b41f46b77608d65569df08b2c3dc5141a1d4b568
shettimax <shettima421@gmail.com>
2025-10-26 21:48:02 +0100

    author block

---
+++ signature.php
@@ -0,0 +1,16 @@
+<?php
+$modifiedDate = date('D.d.M.Y');
+$modifiedTime = date('@h:i A');
+
+echo "<!--
+/***********************************************************
+    AUTHOR:             SHETTIMAX
+    E-MAIL:             shettimax@yahoo.com
+    ORG:                M4XDEV
+    LOCATION:           Nigeria
+    PRODUCT:            MaxCTF
+    FIRST CREATED:      Sat.03.Aug.2020 @07:51 PM
+    LAST MODIFIED:      $modifiedDate $modifiedTime By SHETTIMAX
+************************************************************/
+-->";
+?>
