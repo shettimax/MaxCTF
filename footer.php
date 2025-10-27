5effb3517490e850eb1e2198109559258a941e86
shettimax <shettima421@gmail.com>
2025-10-27 18:59:49 +0100

    bootstrap shit

--- footer.php
+++ footer.php
@@ -11,8 +11,8 @@
 
 </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 </body>
 </html>
